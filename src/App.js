b17fc89ccb11d76667c6fa840d2d981379f8aeff
Bugra Yuksel <bgryks18@gmail.com>
2022-03-13 18:06:13 +0300

    bootstrap projeye dahil edildi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 import './style/style.min.css'
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div>App</div>
 }
 
 export default App
