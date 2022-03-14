fe83adf2189c72e75f3dd8ed35e2a0a91ec4f791
Bugra Yuksel <bgryks18@gmail.com>
2022-03-14 14:54:23 +0300

    arama sonuçları listesi bölümü düzenlendi

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -40,17 +40,57 @@ const index = () => {
           <div className={`${Style.movieSearchResults}`}>
             <h4>Results for "search"</h4>
             <p>top on a movie title to learn more about it</p>
-            <ul>
-              <li>
-                <a href="#">Movie 1</a>
-              </li>
-              <li>
-                <a href="#">Movie 2</a>
-              </li>
-              <li>
-                <a href="#">Movie 3</a>
-              </li>
-            </ul>
+            <div className={`${Style.movieResultListGroup}`}>
+              <ul className={`${Style.movieResultList}`}>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+              </ul>
+              <ul className={`${Style.movieResultList}`}>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+              </ul>
+              <ul className={`${Style.movieResultList}`}>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+              </ul>
+              <ul className={`${Style.movieResultList}`}>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+                <li>
+                  <a href="#">Movie 1</a>
+                </li>
+              </ul>
+            </div>
+            <hr></hr>
+            <small>12 sonuç daha</small>
+            <p>
+              <a href="#">Tümünü gör</a>
+            </p>
           </div>
         </div>
       </div>
